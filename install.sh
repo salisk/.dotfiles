0968d140d7a351e276283a45c6f4cf12afbfd6cf
salisk <sarunas.aliskevicius@gmail.com>
2022-03-31 19:41:57 +0300

    Add osx configs to the install script

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ echo $OSTYPE
 if [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
     echo "Running on Mac OSX..."
+    stow -vv -R -t ~ nvim
+    stow -vv -R -t ~ tmux
+    stow -vv -R -t ~ zsh
 else 
     echo "Running on Linux..."
 fi
