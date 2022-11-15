3a5406ac43976b0568b0c7272af99bc4a23da4d9
salisk <sarunas.aliskevicius@gmail.com>
2022-11-15 19:35:39 +0000

    Update polybar config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,6 +9,7 @@ polybar-msg cmd quit
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar mainbar-i3 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar mainbar-i3-extra 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar mainbar2-i3 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
