b5c6e72f6743f6f6ad453c9dcba4cfb33c403d81
salisk <sarunas.aliskevicius@gmail.com>
2023-08-09 15:08:49 -0700

    Split linux and osx ansible playbooks

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# stow the stow configuration
-stow -vv -t ~ stow
+# show script directory
+current_dir=$(dirname "$BASH_SOURCE")
+echo "Running in $current_dir"
 
 # based on the os
 echo $OSTYPE
 if [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
+    # Mac OSX
     echo "Running on Mac OSX..."
-    stow -vv -R -t ~ nvim
-    stow -vv -R -t ~ tmux
-    stow -vv -R -t ~ zsh
+    if ! which command > /dev/null; then
+        echo "Installing ansible"
+        sudo pip3 install ansible
+    fi
+	
+    echo "Running ansible..."
+    PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}" ansible-playbook -K $current_dir/ansible/osx.yml
 else 
     echo "Running on Linux..."
+
+    echo "Running ansible..."
+    ansible-playbook -K $current_dir/ansible/debian.yml
+
+    # stow the stow configuration
+    stow -vv -t ~ $current_dir/stow
 fi
