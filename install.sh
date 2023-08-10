f30acb71119376005d502e32e75c0341f74f524b
salisk <sarunas.aliskevicius@gmail.com>
2023-08-10 15:21:40 -0700

    Add vscode package

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ echo "Running in $current_dir"
 # based on the os
 echo $OSTYPE
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # Mac OSX
-    echo "Running on Mac OSX..."
-    if ! which ansible > /dev/null; then
-        echo "Installing ansible..."
-        sudo pip3 install ansible
-    fi
-	
-    echo "Running ansible..."
-    PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}" ansible-playbook -K $current_dir/ansible/osx.yml
-else 
-    echo "Running on Linux..."
+	# Mac OSX
+	echo "Running on Mac OSX..."
+	if ! which ansible >/dev/null; then
+		echo "Installing ansible..."
+		sudo pip3 install ansible
+	fi
 
-    echo "Running ansible..."
-    ansible-playbook -K $current_dir/ansible/debian.yml
+	echo "Running ansible..."
+	PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}" ansible-playbook -K $current_dir/ansible/osx.yml
+else
+	echo "Running on Linux..."
 
-    # stow the stow configuration
-    stow -vv -t ~ $current_dir/stow
+	echo "Running ansible..."
+	ansible-playbook -K $current_dir/ansible/debian.yml
+
+	# stow the stow configuration
+	stow -vv -t ~ $current_dir/stow
 fi
