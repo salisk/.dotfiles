7804865b87c92158c1a8000f0ac18cd5f40efd99
salisk <sarunas.aliskevicius@gmail.com>
2023-10-18 23:19:47 +0300

    Update typo

--- sketchybar/.config/sketchybar/items/stats.sh
+++ sketchybar/.config/sketchybar/items/stats.sh
@@ -1,15 +1,15 @@
 sketchybar --add alias "Stats,Disk_mini" right \
-	--set "Stats,Disk_mini" paddng_right=-15 padding_left=-10 label.width=0 \
+	--set "Stats,Disk_mini" padding_right=-15 padding_left=-10 label.width=0 \
 	click_script="open -a /System/Applications/Utilities/Disk\ Utility.app"
 
 sketchybar --add alias "Stats,RAM_mini" right \
-	--set "Stats,RAM_mini" paddng_right=-15 padding_left=-10 label.width=0 \
+	--set "Stats,RAM_mini" padding_right=-15 padding_left=-10 label.width=0 \
 	click_script="open -a /System/Applications/Utilities/Activity\ Monitor.app"
 
 sketchybar --add alias "Stats,CPU_mini" right \
-	--set "Stats,CPU_mini" paddng_right=-15 padding_left=0 label.width=0 \
+	--set "Stats,CPU_mini" padding_right=-15 padding_left=0 label.width=0 \
 	click_script="open -a /System/Applications/Utilities/Activity\ Monitor.app"
 
 sketchybar --add alias "Stats,Network_speed" right \
-	--set "Stats,Network_speed" paddng_right=0 padding_left=0 label.width=0 \
+	--set "Stats,Network_speed" padding_right=0 padding_left=0 label.width=0 \
 	click_script="open -a /System/Applications/Utilities/Activity\ Monitor.app"
