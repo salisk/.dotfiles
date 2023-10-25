b2710e15c682edaf7f68b1edb2abc0c0179a2cb2
salisk <sarunas.aliskevicius@gmail.com>
2023-10-25 17:27:40 +0300

    Change sketchybar network icon

--- sketchybar/.config/sketchybar/items/network.sh
+++ sketchybar/.config/sketchybar/items/network.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env sh
 
+source "$CONFIG_DIR/icons.sh"
+
 sketchybar -m --add item network.up right \
-              --set network.up label.font="$FONT:Bold:9.0" \
-                               icon.font="$FONT:Bold:12.0" \
-                               icon=􀆇 \
-                               icon.highlight_color=$BLUE \
-                               y_offset=5 \
-                               width=0 \
-                               update_freq=1 \
-                               script="$PLUGIN_DIR/network.sh" \
-\
-              --add item network.down right \
-              --set network.down label.font="$FONT:Bold:9.0" \
-                                 icon.font="$FONT:Bold:12.0" \
-                                 icon=􀆈 \
-                                 icon.highlight_color=$YELLOW \
-                                 y_offset=-5 \
-                                 update_freq=1\
+	--set network.up label.font="$FONT:Bold:10" \
+	icon.font="$NERD_FONT:Bold:16.0" \
+	icon="$NETWORK_UP" \
+	icon.highlight_color=$BLUE \
+	y_offset=5 \
+	width=0 \
+	update_freq=1 \
+	script="$PLUGIN_DIR/network.sh" \
+	\
+	--add item network.down right \
+	--set network.down label.font="$FONT:Bold:10" \
+	icon.font="$NERD_FONT:Bold:16.0" \
+	icon="$NETWORK_DOWN" \
+	icon.highlight_color=$YELLOW \
+	y_offset=-5 \
+	update_freq=1
+
--- sketchybar/.config/sketchybar/plugins/network.sh
+++ sketchybar/.config/sketchybar/plugins/network.sh
@@ -5,17 +5,17 @@ UP=$(echo $UPDOWN | awk "{ print \$2 }" | cut -f1 -d ".")
 
 DOWN_FORMAT=""
 if [ "$DOWN" -gt "999" ]; then
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%3.0f Mbps", $1 / 1000}')
+	DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f Mbps", $1 / 1000}')
 else
-  DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%3.0f kbps", $1}')
+	DOWN_FORMAT=$(echo $DOWN | awk '{ printf "%03.0f kbps", $1}')
 fi
 
 UP_FORMAT=""
 if [ "$UP" -gt "999" ]; then
-  UP_FORMAT=$(echo $UP | awk '{ printf "%3.0f Mbps", $1 / 1000}')
+	UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f Mbps", $1 / 1000}')
 else
-  UP_FORMAT=$(echo $UP | awk '{ printf "%3.0f kbps", $1}')
+	UP_FORMAT=$(echo $UP | awk '{ printf "%03.0f kbps", $1}')
 fi
 
 sketchybar --set network.down label="$DOWN_FORMAT" icon.highlight=$(if [ "$DOWN" -gt "0" ]; then echo "on"; else echo "off"; fi) \
-           --set network.up label="$UP_FORMAT" icon.highlight=$(if [ "$UP" -gt "0" ]; then echo "on"; else echo "off"; fi)
+	--set network.up label="$UP_FORMAT" icon.highlight=$(if [ "$UP" -gt "0" ]; then echo "on"; else echo "off"; fi)
