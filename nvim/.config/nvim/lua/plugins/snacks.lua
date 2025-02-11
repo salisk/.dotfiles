185dd4837c917a3834182b26cd80aa6c14bc6976
salisk <sarunas.aliskevicius@gmail.com>
2025-02-11 14:55:44 +0200

    Adjust snacks picker file truncation

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      formatters = {
+        file = {
+          filename_first = true,
+          truncate = 150,
+        },
+      },
+    },
+  },
+}
