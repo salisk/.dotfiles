f7f923fc0e813d01428cb6c2e254ed04fcb44625
salisk <sarunas.aliskevicius@gmail.com>
2026-08-25 16:24:32 +0300

    chore: remove nvim obsidian completion override

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -30,7 +30,6 @@ return {
   opts = {
     legacy_commands = false,
     -- attachments = { img_folder = obsidian_relative_path .. "/_resources" },
-    completion = { nvim_cmp = false },
     templates = { folder = obsidian_relative_path .. "/Templates", date_format = "%y%m%d", time_format = "%H%M" },
     workspaces = {
       {
